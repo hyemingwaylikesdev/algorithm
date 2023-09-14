9e8be6cebb4039da6dd7e4531ca88db1e59122ba
hyeminpark <hyeminpark@218.38.137.28>
2023-09-14 18:43:41 +0900

    봉우리 문제 - 문자열

--- algorithm/src/algorithm/봉우리.java
+++ algorithm/src/algorithm/봉우리.java
@@ -1,5 +1,48 @@
 package algorithm;
 
-public class 봉우리 {
+import java.util.Scanner;
 
+public class 봉우리 {
+	int[] dx = { -1, 0, 1, 0 };
+	int[] dy = { 0, 1, 0, -1 };
+
+	public int solution(int n, int[][] arr) {
+
+		int answer = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				boolean flag = true;
+				//4번 돌리기 (상하좌우) 
+				for (int k = 0; k < 4; k++) {
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+
+					// 경계선 outofBoundary 에러처리:
+					if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+						flag = false;
+						break;
+					}
+
+				}
+				if (flag)
+					answer++;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String arg[]) {
+		봉우리 T = new 봉우리();
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print(T.solution(n, arr));
+
+	}
 }
